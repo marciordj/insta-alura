a8bae337cb1754e0aa9f35cb30bf5beaea861198
Marcio Rodrigues <lmarciorodriguesjunior@gmail.com>
2020-03-12 11:31:32 -0300

    refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -3,66 +3,27 @@ import React, { Fragment } from 'react';
 import { FlatList, ScrollView } from 'react-native';
 
 import Batman from './assets/img/Batman.jpg';
-import batPost from './assets/img/batPost.jpg';
-import MestreMago from './assets/img/mestreMago.jpg';
-import magoPost from './assets/img/mestrePost.jpg';
-import spiderPost from './assets/img/spider.jpg';
-import SpiderMan from './assets/img/spiderPost.jpg';
+import MasterMage from './assets/img/mestreMago.jpg';
+import Spider from './assets/img/spider.jpg';
 import Header from './components/Header';
-import Posts from './components/Posts';
-
-
-interface IUser {
-  id: number;
-  name: string;
-  desc: string;
-  photo: any;
-  postImg: any;
-}
+import Post from './components/Posts';
 
 const App = () => {
-  const infoUsers = [
-    {
-      id: 1,
-      name: 'Bruce',
-      photo: Batman,
-      desc:
-        'Mussum Ipsum, cacilds vidis litro abertis. Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Suco de cevadiss deixa as pessoas mais interessantis. Paisis, filhis, espiritis santis. Viva Forevis aptent taciti sociosqu ad litora torquent.',
-      postImg: batPost,
-    },
-    {
-      id: 2,
-      name: 'Mestre dos Magos',
-      photo: MestreMago,
-      desc:
-        'Mussum Ipsum, cacilds vidis litro abertis. Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Suco de cevadiss deixa as pessoas mais interessantis. Paisis, filhis, espiritis santis. Viva Forevis aptent taciti sociosqu ad litora torquent.',
-      postImg: magoPost,
-    },
-    {
-      id: 3,
-      name: 'Peter',
-      photo: SpiderMan,
-      desc:
-        'Mussum Ipsum, cacilds vidis litro abertis. Suco de cevadiss, é um leite divinis, qui tem lupuliz, matis, aguis e fermentis. Suco de cevadiss deixa as pessoas mais interessantis. Paisis, filhis, espiritis santis. Viva Forevis aptent taciti sociosqu ad litora torquent.',
-      postImg: spiderPost,
-    },
+  const userInfo = [
+    {id: 1, name: 'Peter', photo: Spider, desc: 'blablablablablablablabla' },
+    {id: 2, name: 'Bruce', photo: Batman, desc: 'blabalablablabalablaba' },
+    {id: 3, name: 'Master Mage', photo: MasterMage, desc: 'blabalablablabla'},
   ];
-
-  function Item({name, photo, desc, postImg}: IUser) {
-    return (
-      <Fragment>
-        <Header userName={name} postImg={postImg} personalPhoto={photo} />
-        <Posts postPhoto={photo} postImg={postImg} postDescribe={desc} />
-      </Fragment>
-    );
-  }
 
   return (
     <ScrollView>
       <FlatList
-        data={infoUsers}
+        data={userInfo}
         renderItem={({item}) => (
-          <Item name={item.name} photo={item.photo} desc={item.desc} postImg={item.postImg} />
+          <Fragment>
+            <Header userName={item.name} personalPhoto={item.photo} />
+            <Post postImg={item.photo} postDescribe={item.desc}/>
+          </Fragment>
         )}
         keyExtractor={item => item.id.toString()}
       />
